165db81ee12379d4ce14411430301698869ef568
Shagarbek <shagarbekov@gmail.com>
2025-10-04 19:38:50 +0500

    test(algorithm): comprehensive test suite with edge cases

--- src/test/java/algorithms/MinHeapTest.java
+++ src/test/java/algorithms/MinHeapTest.java
@@ -2,60 +2,71 @@ package algorithms;
 
 import metrics.PerformanceTracker;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MinHeapTest {
-    @Test
-    void testInsertAndExtractMin() {
-        PerformanceTracker tracker = new PerformanceTracker();
-        MinHeap heap = new MinHeap(5, tracker);
+public class MinHeapTest {
 
-        heap.insert(10);
-        heap.insert(3);
+    @Test
+    void testInsertAndExtractSingleElement() {
+        PerformanceTracker tracker = new PerformanceTracker();
+        MinHeap heap = new MinHeap(10, tracker);
+
+        heap.insert(42);
+        assertEquals(1, heap.getSize());
+
+        int min = heap.extractMin();
+        assertEquals(42, min);
+        assertEquals(0, heap.getSize());
+    }
+
+    @Test
+    void testInsertMultipleElements() {
+        PerformanceTracker tracker = new PerformanceTracker();
+        MinHeap heap = new MinHeap(10, tracker);
+
         heap.insert(5);
+        heap.insert(3);
+        heap.insert(7);
 
         assertEquals(3, heap.extractMin());
         assertEquals(5, heap.extractMin());
-        assertEquals(10, heap.extractMin());
+        assertEquals(7, heap.extractMin());
     }
 
     @Test
-    void testDecreaseKey() {
+    void testExtractFromEmptyHeap() {
         PerformanceTracker tracker = new PerformanceTracker();
-        MinHeap heap = new MinHeap(5, tracker);
+        MinHeap heap = new MinHeap(10, tracker);
 
-        heap.insert(20);
-        heap.insert(15);
-        heap.insert(30);
-
-        heap.decreaseKey(2, 5);
-        assertEquals(5, heap.extractMin());
+        assertThrows(Exception.class, heap::extractMin);
     }
 
     @Test
-    void testMerge() {
+    void testWithDuplicates() {
         PerformanceTracker tracker = new PerformanceTracker();
-        MinHeap h1 = new MinHeap(5, tracker);
-        MinHeap h2 = new MinHeap(5, tracker);
+        MinHeap heap = new MinHeap(10, tracker);
 
-        h1.insert(10);
-        h1.insert(2);
-        h2.insert(5);
-        h2.insert(1);
+        heap.insert(4);
+        heap.insert(4);
+        heap.insert(4);
 
-        MinHeap merged = MinHeap.merge(h1, h2);
-        assertEquals(1, merged.extractMin());
-        assertEquals(2, merged.extractMin());
+        assertEquals(4, heap.extractMin());
+        assertEquals(4, heap.extractMin());
+        assertEquals(4, heap.extractMin());
     }
 
     @Test
-    void testEdgeCases() {
+    void testHeapGrowth() {
         PerformanceTracker tracker = new PerformanceTracker();
         MinHeap heap = new MinHeap(2, tracker);
 
-        assertThrows(Exception.class, heap::extractMin);
+        for (int i = 0; i < 100; i++) {
+            heap.insert(i);
+        }
 
-        heap.insert(42);
-        assertEquals(42, heap.peekMin());
+        assertEquals(100, heap.getSize());
+        assertEquals(0, heap.extractMin());
+        assertEquals(1, heap.extractMin());
     }
 }
