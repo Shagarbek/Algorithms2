54dc00a5b8b8c9d6cf1b92b65f8812427ffdcb9b
Shagarbek <shagarbekov@gmail.com>
2025-10-05 18:32:37 +0500

    fix(edge-cases): handle empty and single-element heaps properly

--- src/test/java/algorithms/MinHeapTest.java
+++ src/test/java/algorithms/MinHeapTest.java
@@ -9,11 +9,9 @@ class MinHeapTest {
     void testInsertAndExtractMin() {
         PerformanceTracker tracker = new PerformanceTracker();
         MinHeap heap = new MinHeap(5, tracker);
-
         heap.insert(10);
         heap.insert(3);
         heap.insert(5);
-
         assertEquals(3, heap.extractMin());
         assertEquals(5, heap.extractMin());
         assertEquals(10, heap.extractMin());
@@ -23,11 +21,9 @@ class MinHeapTest {
     void testDecreaseKey() {
         PerformanceTracker tracker = new PerformanceTracker();
         MinHeap heap = new MinHeap(5, tracker);
-
         heap.insert(20);
         heap.insert(15);
         heap.insert(30);
-
         heap.decreaseKey(2, 5);
         assertEquals(5, heap.extractMin());
     }
@@ -37,12 +33,10 @@ class MinHeapTest {
         PerformanceTracker tracker = new PerformanceTracker();
         MinHeap h1 = new MinHeap(5, tracker);
         MinHeap h2 = new MinHeap(5, tracker);
-
         h1.insert(10);
         h1.insert(2);
         h2.insert(5);
         h2.insert(1);
-
         MinHeap merged = MinHeap.merge(h1, h2);
         assertEquals(1, merged.extractMin());
         assertEquals(2, merged.extractMin());
@@ -52,9 +46,7 @@ class MinHeapTest {
     void testEdgeCases() {
         PerformanceTracker tracker = new PerformanceTracker();
         MinHeap heap = new MinHeap(2, tracker);
-
         assertThrows(Exception.class, heap::extractMin);
-
         heap.insert(42);
         assertEquals(42, heap.peekMin());
     }
